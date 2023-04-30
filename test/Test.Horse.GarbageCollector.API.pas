b44f90923167ed16623b218f6a4ab99bfc063efe
Iago César Fernandes Nogueira <iagocesar.nogueira@gmail.com>
2023-04-30 19:22:44 -0300

    Primeiro testes ok

--- test/Test.Horse.GarbageCollector.API.pas
+++ test/Test.Horse.GarbageCollector.API.pas
@@ -20,9 +20,18 @@ type
     [TearDownFixture]
     procedure TearDownFixture;
 
+    [Test]
+    procedure Test_ArquivoCriadoSeraDeletado;
+
   end;
 
 implementation
+
+uses
+  System.JSON,
+  System.SysUtils,
+  REST.Json,
+  DTO.Arquivo;
 
 { TMyTestObject }
 
@@ -42,6 +51,39 @@ procedure TMyTestObject.TearDownFixture;
 begin
   FApp.Stop;
   FApp.Free;
+end;
+
+procedure TMyTestObject.Test_ArquivoCriadoSeraDeletado;
+var LResponse : IResponse; LBody: TJSONObject; LDTOArquivo: TDTOArquivo;
+begin
+  try
+    LDTOArquivo := TDTOArquivo.Create;
+    LDTOArquivo.Nome := 'teste-auto';
+    LDTOArquivo.Conteudo := 'teste automatizado';
+
+    LResponse := BaseRequest
+      .Resource('/criar-arquivo')
+      .AddBody(TJson.ObjectToJsonString(LDTOArquivo))
+      .Post();
+
+  finally
+    LDTOArquivo.Free;
+  end;
+
+  Assert.AreEqual(201, LResponse.StatusCode);
+
+  LBody := TJSONObject.ParseJSONValue(LResponse.Content) as TJSONObject;
+  try
+    // Indica que quando ainda no controller o arquivo existia
+    Assert.IsTrue(LBody.GetValue<Boolean>('exists'));
+
+    // Após Response.Send no controller, o Middleware deletará o arquivo
+    Assert.IsFalse(
+      FileExists(LBody.GetValue<string>('path'))
+    );
+  finally
+    LBody.Free;
+  end;
 end;
 
 initialization
